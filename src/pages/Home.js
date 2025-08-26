5d0fa9a8a40d62a7c72569880bb956597b8b8084
bryson k echols <xdrcybersecurity@proton.me>
2025-08-26 13:03:14 -0700

    first commit
    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit.Lorem Ipsum dolor sit amet, consectetur adipiscing elit.Lorem Ipsum dolor sit amet, consectetur adipiscing elit.Lorem Ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
