711e0966bdb8a7c04afaa4f3c6f50ec9e197dcb9
ebk1996 <echolsbrysonkyle@gmail.com>
2025-08-27 15:06:40 -0700

    forms and actions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 //pages
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/help/Contact'
+import Contact, { contactAction } from './pages/help/Contact'
 import Faq from './pages/help/Faq'
 import CareerDetails, { careerDetailsLoader } from './pages/careers/CareerDetails'
 import NotFound from './pages/NotFound'
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
 
       <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />}>
