8e53e3c5c33af492901816b5ced84ab17eb9839a
ebk1996 <echolsbrysonkyle@gmail.com>
2025-08-27 15:28:24 -0700

    About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,13 @@
+import { useState } from "react"  
+import { Navigate } from "react-router-dom"
+
 export default function About () {
+    const [user, setUser] = useState('mario')
+    
+if(!user) {
+    return <Navigate to="/" replace={true}/>
+} 
+
     return (
         <div>
             <h2>About Us</h2>
@@ -6,6 +15,7 @@ export default function About () {
             <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button onClick={() => setUser(null)}>Logout</button>
         </div>
     )
 }
